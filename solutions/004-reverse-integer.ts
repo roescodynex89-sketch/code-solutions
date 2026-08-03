9288cb54a157170ef2c4ffde99c90426f565e303
roescodynex89-sketch <roescody.nex89@gmail.com>
2026-08-03 16:16:44 +0600

    fixed test

--- solutions/004-reverse-integer.ts
+++ solutions/004-reverse-integer.ts
@@ -47,7 +47,7 @@ function reverse(x: number): number {
 
 // Test Cases
 console.log(reverse(123));          // 321
-console.log(reverse(-123));         // -321
+
 console.log(reverse(120));          // 21
 console.log(reverse(0));            // 0
 console.log(reverse(1534236469));   // 0 (Overflow)
